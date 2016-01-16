35866475cee6abe461056c78e7bb3c1cb7da298f
Dheeraj Joshi <dheerajjoshi1991@gmail.com>
2016-01-16 19:26:18 +0530

    Add Url Decoder & Encoder tool

---
+++ scripts/decode.js
@@ -0,0 +1,38 @@
+(function() {
+	'use strict';
+
+	function setEditorValue(val) {
+		var editor = document.getElementById('editor');
+		editor.value = val;
+	}
+
+	function getEditorValue() {
+		var editor = document.getElementById('editor');
+		return editor.value || '';
+	}
+
+	function decode(url) {
+		return decodeURIComponent(url);
+	}
+
+	function encode(url) {
+		return encodeURIComponent(url);
+	}
+
+	// Set default value
+	setEditorValue(document.URL);
+
+	var decoder = document.getElementById('btn-decode');
+	var encoder = document.getElementById('btn-encode');
+
+	// Add Event Listeners
+	decoder.addEventListener('click', function() {
+		var url = getEditorValue();
+		setEditorValue(decode(url));
+	});
+
+	encoder.addEventListener('click', function() {
+		var url = getEditorValue();
+		setEditorValue(encode(url));
+	});
+})();
